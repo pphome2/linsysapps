16a6a7c674cac597a563e84a88f608cf9b9b984c
Péter <pphome2@gmail.com>
2026-07-02 10:15:23 +0200

    20260702

--- config/pipe.sh
+++ config/pipe.sh
@@ -1,9 +1,9 @@
 # 1. Állítsunk le mindent teljesen
 systemctl --user stop pipewire.service pipewire.socket pipewire-pulse.service pipewire-pulse.socket wireplumber.service
-
+sleep 5
 # 2. Töröljük a "start-limit-hit" hibaállapotot (ez reseteli a számlálót)
 systemctl --user reset-failed pipewire.service pipewire.socket pipewire-pulse.service pipewire-pulse.socket wireplumber.service
-
+sleep 5
 # 3. Indítsuk el először a socketeket és a főbb szolgáltatásokat tiszta lappal
 systemctl --user start pipewire.socket pipewire-pulse.socket
 systemctl --user start pipewire.service pipewire-pulse.service wireplumber.service
